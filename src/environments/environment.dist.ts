6334fcdcf15dd184e968db905585ebb10b432e82
unknown <k173634@nu.edu.pk>
2019-07-08 12:34:38 +0500

    Nothing(2)

---
+++ src/environments/environment.dist.ts
@@ -0,0 +1,18 @@
+/*
+  This is the template environment with dummy API keys.
+*/
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>',
+    appId: '<your-app-id>'
+  },
+  googleMap: {
+    apiKey: '<your-key>'
+  }
+};
