8d71feb7b14a2b5f4201fb25b5d250700efe78bb
unknown <k173634@nu.edu.pk>
2019-07-08 16:04:00 +0500

    Cash Accounts page Updated Successfull (check for popover menu)

--- src/app/members/accounts/cash/cash.page.ts
+++ src/app/members/accounts/cash/cash.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from 'src/app/services/database.service';
+import { CashAccount, BankAccount } from 'src/app/services/helper-classes';
+import { PopoverController } from '@ionic/angular';
+import { EditComponent } from '../bank/edit/edit.component';
 
 @Component({
   selector: 'app-cash',
@@ -8,8 +11,13 @@ import { DatabaseService } from 'src/app/services/database.service';
 })
 export class CashPage implements OnInit {
 
+  // For from submission purposes only (new entries)
+  particulars: string;
+  currentBalance: number;
+  
   constructor(
-    private db: DatabaseService
+    private db: DatabaseService,
+    private poc: PopoverController
   ) { }
 
   ngOnInit() {
@@ -19,4 +27,27 @@ export class CashPage implements OnInit {
     return this.db.cashAccounts;
   }
 
+  addCashAccount() {
+    this.db.cashAccounts.push(new CashAccount(this.particulars, this.currentBalance));
+  }
+
+  editCashAccount() {
+
+  }
+
+  get entryTypes() {
+    return this.db.entryTypes;
+  }
+
+  async presentPopover(cashAccount: CashAccount) {
+    const popover = await this.poc.create({
+      component: EditComponent,
+      componentProps: {
+        cashAccount
+      }
+    });
+    return await popover.present();
+  }
+
 }
+
