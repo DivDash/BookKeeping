3908fab18589f1ca3b883a66d6559e7c79343a55
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-08-02 00:42:13 +0500

    Accidentally restructured database

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -31,4 +31,30 @@ export class ServerService {
     });
     return collectionBehavior;
   }
+
+  // Creates document with element
+  // TODO: Handle error if no internet
+  async createDocument(name: string, element: object) {
+    return await new Promise((resolve, reject) =>
+    this.http.post(`${serverUrl}/${name}/create-${name}`, element)
+    .subscribe(resolve, reject));
+  }
+
+  // Updates document of provided id with element
+  // TODO: Handle error if no internet
+  async updateDocument(name: string, id: string, element: object) {
+    return await new Promise((resolve, reject) =>
+    this.http.put(`${serverUrl}/${name}/update-${name}/${id}`, element)
+    .subscribe(resolve, reject));
+  }
+
+  // Delete document using id
+  // TODO: Handle error if no internet
+  // Also depends on whether internet was available when object was created
+  async deleteDocument(name: string, id: string) {
+    return await new Promise((resolve, reject) =>
+    this.http.delete(`${serverUrl}/${name}/delete-${name}/${id}`)
+    .subscribe(resolve, reject));
+  }
+
 }
