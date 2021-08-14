6c1bfe54bb51da565a77834d4336f20cf63f05ae
Zain Haider <zhaider222000@gmail.com>
2021-08-14 18:12:58 +0500

    JWT TOKEN

--- expressBackend/models/schema.js
+++ expressBackend/models/schema.js
@@ -31,8 +31,34 @@ const table=new mongoose.Schema({
         type:String,
         required:true
 
+    },
+    tokens:[
+        {   
+            token:{
+            type:String,
+            required:true
+
+        }
     }
+    ]
+
 })
+
+
+table.methods.generateauthtoken=async function (){
+    try{
+        let token=jwt.sign({_id:this._id},'BookKeeping')
+        this.tokens=this.tokens.concat({token:token})
+        await this.save()
+        return token
+
+    }catch(err){
+        consol.log(err)
+    }
+    
+
+}
+
 
 const user=mongoose.model('REGISTRATION',table)
 
--- expressBackend/routes/routing.js
+++ expressBackend/routes/routing.js
@@ -5,17 +5,16 @@ const user = require( '../models/schema' )
 
 
 router.post( '/registration', async ( req, res ) => {
-    try{
+    try {
 
-    const { name, email, password, confirm, phone, work } = req.body
-    console.log( name, email, password, confirm, phone, work)
+        const { name, email, password, confirm, phone, work } = req.body
 
-    if ( !name || !email || !password || !confirm || !phone || !work ) {
-        res.status( 422 ).json( { error: "FILL THE FULL FORM" } )
-    }
-    if ( password !== confirm ) {
-        res.status( 422 ).json( { error: "CONFIRM PASSWORD DOSENT MATCH" } )
-    }
+        if ( !name || !email || !password || !confirm || !phone || !work ) {
+            res.status( 422 ).json( { error: "FILL THE FULL FORM" } )
+        }
+        if ( password !== confirm ) {
+            res.status( 422 ).json( { error: "CONFIRM PASSWORD DOSENT MATCH" } )
+        }
         const userexist = await user.findOne( { email: email } )
         if ( userexist ) {
             res.status( 422 ).json( { message: "Email Already Exist" } )
@@ -23,10 +22,45 @@ router.post( '/registration', async ( req, res ) => {
         const saving = new user( { name, email, password, confirm, phone, work } );
         await saving.save()
         res.status( 201 ).json( { message: "REGSITERED SUCCESFULLY" } )
-    } catch ( err ) {
+    }
+    catch ( err ) {
         console.log( err )
         res.send( "there is error" )
     }
 } )
+
+
+router.post( '/signin', async ( req, res ) => {
+    try 
+    {
+        let { email, password } = req.body
+
+        if ( !email || !password ) {
+            res.status( 422 ).json( { error: "FILL THE FULL FORM" } )
+        }
+
+        const userexist = await user.findOne( { email: email } )
+        if ( userexist ) {
+            const match = await bcrypt.compare( password, userexist.password );
+            const token = await userexist.generateauthtoken();
+            res.cookie( "BookKeeping", token, {
+                expires: new Date( Date.now() + 864000000 ),
+                httpOnly: true
+            } );
+            if ( match ) {
+                res.json( { message: "loggin succesfully" } )
+            }
+            if ( !match ) {
+                res.json( { err: "invalid credentials" } )
+            }
+            else {
+                res.json( { err: "invalid credentials" } )
+            }
+        }
+    } catch ( err ) {
+        res.send( "notokk" )
+    }
+} )
+
 
 module.exports = router;
