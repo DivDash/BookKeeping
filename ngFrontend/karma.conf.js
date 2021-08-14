2294d193bc8fa42741cff1b78ac18d680befd7ea
Zain Haider <zhaider222000@gmail.com>
2021-08-14 16:02:20 +0500

    Frontend Enviroment Error
    Try 1

--- ngFrontend/karma.conf.js
+++ ngFrontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Book-Keeper'),
+      dir: require('path').join(__dirname, './coverage/ngFrontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
